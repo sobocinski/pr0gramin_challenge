d9a9c67778e30eb83e5a362b6ee441b6ae35c3ca
Paweł Sobociński <pawel@agudo.pl>
2014-10-08 23:32:19 +0200

    01 init

---
+++ 01/01/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  01
+//
+//  Created by Pawel Sobocinski on 08.10.2014.
+//  Copyright (c) 2014 Pawel Sobocinski. All rights reserved.
+//
+
+import Foundation
+
+
+enum cardType: Character {
+    case BlackSpade = "\u{2660}"
+    case RedHeart = "\u{2665}"
+    case RedDiamond = "\u{2666}"
+    case BlackClub = "\u{2663}"
+}
+
+let cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+
+
+
+
+println(cardType)
+
